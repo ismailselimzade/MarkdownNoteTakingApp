28212249e3f60cc978d0c6a8129a2730295cf6e7
Ismayil Selimzade <ismailselimzade@mail.ru>
2026-07-26 09:30:35 +0400

    feat: configure AppDbContext with Fluent API relationships

---
+++ MarkdownNoteTakingApp/Data/AppDbContext.cs
@@ -0,0 +1,28 @@
+﻿using MarkdownNoteTakingApp.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace MarkdownNoteTakingApp.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<Note> Notes { get; set; }
+        public DbSet<Attachment> Attachments { get; set; }
+
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Note>()
+                .HasOne(n => n.User)
+                .WithMany(u => u.Notes)
+                .HasForeignKey(n => n.UserId);
+
+            modelBuilder.Entity<Attachment>()
+                .HasOne(a => a.Note)
+                .WithMany(n => n.Attachments)
+                .HasForeignKey(a => a.NoteId);
+        }
+    }
+}
