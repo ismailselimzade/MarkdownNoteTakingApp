a5b7482086162eb5df4df17dc20eaeaf456ff56a
Ismayil Selimzade <ismailselimzade@mail.ru>
2026-07-28 10:56:07 +0400

    feat: add password hashing service with BCrypt

--- MarkdownNoteTakingApp/Program.cs
+++ MarkdownNoteTakingApp/Program.cs
@@ -1,4 +1,5 @@
 using MarkdownNoteTakingApp.Data;
+using MarkdownNoteTakingApp.Services;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -14,6 +15,7 @@ builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 
+builder.Services.AddScoped<IPasswordService, PasswordService>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
---
+++ MarkdownNoteTakingApp/Services/IPasswordService.cs
@@ -0,0 +1,8 @@
+﻿namespace MarkdownNoteTakingApp.Services
+{
+    public interface IPasswordService
+    {
+        public string HashPassword(string password);
+        public bool VerifyPassword(string password, string hash);
+    }
+}
